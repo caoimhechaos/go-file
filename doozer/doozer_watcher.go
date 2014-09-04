cbcab30eb6b98da95588bd11a02d26ca2ed37732
Caoimhe Chaos <caoimhechaos@protonmail.com>
2014-09-04 23:53:03 +0200

    Create a registry for file watchers.
    Add a Doozer implementation to it.

---
+++ doozer/doozer_watcher.go
@@ -0,0 +1,126 @@
+/*
+ * (c) 2014, Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *	     Starship Factory. All rights reserved.
+ *
+ * Redistribution and use in source  and binary forms, with or without
+ * modification, are permitted  provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of  source code  must retain the  above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this  list of conditions and the  following disclaimer in
+ *   the  documentation  and/or  other  materials  provided  with  the
+ *   distribution.
+ * * Neither  the name  of the Starship Factory  nor the  name  of its
+ *   contributors may  be used to endorse or  promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS"  AND ANY EXPRESS  OR IMPLIED WARRANTIES  OF MERCHANTABILITY
+ * AND FITNESS  FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT
+ * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package doozer
+
+import (
+	"bytes"
+	"io"
+	"net/url"
+
+	"github.com/caoimhechaos/go-file"
+	"github.com/ha/doozer"
+)
+
+// Watcher for an individual Doozer file (or a subtree).
+type DoozerWatcher struct {
+	doozer_conn *doozer.Conn
+	path        string
+	errchan     chan error
+	shutdown    bool
+	cb          func(string, io.ReadCloser)
+}
+
+// File handler integration for the DoozerWatcher class.
+type DoozerWatcherCreator struct {
+	conn *doozer.Conn
+}
+
+func (d *DoozerWatcherCreator) Watch(file *url.URL, cb func(string, io.ReadCloser), errchan chan error) (file.Watcher, error) {
+	var watcher *DoozerWatcher = NewDoozerWatcher(d.conn, file.Path, cb, errchan)
+	return watcher, nil
+}
+
+// Register the Doozer watcher with the go-file mechanisms.
+func RegisterFileType(conn *doozer.Conn) {
+	var watcher_creator = &DoozerWatcherCreator{
+		conn: conn,
+	}
+	file.RegisterWatcher("doozer", watcher_creator)
+	file.RegisterWatcher("dz", watcher_creator)
+}
+
+// Create a new Doozer watcher on the connection "conn". Listen for changes
+// of the file / subtree "path", and deliver notifications to "callback".
+// Any errors will be returned on the given error channel "errchan.
+//
+// The callback function will receive the path of the actually modified file
+// as the first parameter and a ReadCloser with the files contents as the
+// second. The ReadCloser does not do any significant work until Read() is
+// invoked for the first time, so it is safe to ignore it and just use this
+// to be notified of file modifications.
+func NewDoozerWatcher(conn *doozer.Conn, path string,
+	cb func(string, io.ReadCloser), errchan chan error) *DoozerWatcher {
+	var ret = &DoozerWatcher{
+		doozer_conn: conn,
+		path:        path,
+		errchan:     errchan,
+	}
+	go ret.watchForChanges()
+	return ret
+}
+
+// Watch for changes on the specified pattern.
+func (dw *DoozerWatcher) watchForChanges() {
+	var rev int64
+	var err error
+	for {
+		var ev doozer.Event
+		var buf *bytes.Reader
+
+		if dw.shutdown {
+			return
+		}
+
+		ev, err = dw.doozer_conn.Wait(dw.path, rev)
+		if err != nil {
+			dw.errchan <- err
+			continue
+		}
+
+		// If the operation is not SET, it probably won't contain
+		// any new data to set.
+		if !ev.IsSet() {
+			continue
+		}
+
+		buf = bytes.NewReader(ev.Body)
+		go dw.cb(ev.Path, file.NewReadCloserFake(buf))
+
+		rev = ev.Rev
+	}
+}
+
+// Shut down the listener after the next change.
+// There's no way to stop it immediately, though.
+func (dw *DoozerWatcher) Shutdown() {
+	dw.shutdown = true
+}
---
+++ watcher_registry.go
@@ -0,0 +1,79 @@
+/*
+ * (c) 2014, Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *	     Starship Factory. All rights reserved.
+ *
+ * Redistribution and use in source  and binary forms, with or without
+ * modification, are permitted  provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of  source code  must retain the  above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this  list of conditions and the  following disclaimer in
+ *   the  documentation  and/or  other  materials  provided  with  the
+ *   distribution.
+ * * Neither  the name  of the Starship Factory  nor the  name  of its
+ *   contributors may  be used to endorse or  promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS"  AND ANY EXPRESS  OR IMPLIED WARRANTIES  OF MERCHANTABILITY
+ * AND FITNESS  FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT
+ * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package file
+
+import (
+	"errors"
+	"io"
+	"net/url"
+)
+
+// Objects describing how to watch a specific type of files, identified
+// by their URL schema. Watch will be invoked whenever watching a file
+// with the given schema is requested.
+type WatcherCreator interface {
+	Watch(*url.URL, func(string, io.ReadCloser), chan error) (Watcher, error)
+}
+
+// Watchers are the objects doing the actual watching of individual
+// files. They are configured by the WatcherCreator and will continue
+// invoking their configured handlers until Shutdown() is called.
+type Watcher interface {
+	Shutdown()
+}
+
+// List of URL schema handlers known.
+var fileWatcherHandlers map[string]WatcherCreator = make(map[string]WatcherCreator)
+
+// Register "creator" as a handler for watchers for all URLs with the given
+// "schema".
+func RegisterWatcher(schema string, creator WatcherCreator) {
+	fileWatcherHandlers[schema] = creator
+}
+
+// Watch the given "fileurl" for changes, sending all of them to the specified
+// "handler". This will look up the required handler for the scheme specified
+// in the URL and forward the watch request. A Watcher object is returned
+// which can be used to stop watching, as defined by the individual watchers.
+func Watch(fileurl *url.URL, handler func(string, io.ReadCloser),
+	errchan chan error) (Watcher, error) {
+	var creator WatcherCreator
+	var ok bool
+
+	creator, ok = fileWatcherHandlers[fileurl.Scheme]
+	if !ok {
+		return nil, errors.New("No handler registered for \"" +
+			fileurl.Scheme + "\"")
+	}
+
+	return creator.Watch(fileurl, handler, errchan)
+}
